5b1b4ff432c26f2d9332476c4001b60a4256a1af
JohnBartlett <JohnBartlett@users.noreply.github.com>
2025-07-28 19:20:31 +0000

    Pass 2

--- api/scraper.js
+++ api/scraper.js
@@ -10,13 +10,13 @@ module.exports = async (req, res) => {
   const page = await browser.newPage();
 
   // (Uncomment/adapt if login required)
-  /*
+  
   await page.goto('https://www.colwright.com/login');
   await page.type('input[name="username"]', process.env.COLWRIGHT_USERNAME);
   await page.type('input[name="password"]', process.env.COLWRIGHT_PASSWORD);
   await page.click('button[type="submit"]');
   await page.waitForNavigation();
-  */
+  
 
   await page.goto('https://www.colwright.com/inventory', { waitUntil: 'networkidle' });
 
