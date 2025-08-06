ec4559be110170d6b8fc5df2132334aaf8d508d6
John Bartlett <john@example.com>
2025-08-06 07:37:31 -0400

    v1.13.3: Update startup scripts and add ngrok config

--- stop-server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Stopping Hamilton Estate Inventory System...
-echo.
-echo If you see a Windows popup asking about default programs, click "More apps" and select "Windows PowerShell"
-echo.
-powershell -ExecutionPolicy Bypass -File "stop-server.ps1"
-echo.
-echo Press any key to exit...
-pause >nul 
